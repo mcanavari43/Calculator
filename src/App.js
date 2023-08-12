0578c924942a9f932c026b6e33ae7aa0de85d49e
mcanavari43 <manuel.canavari43@gmail.com>
2023-08-12 21:51:11 +0200

    Restyling

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import Button from './componentes/Button';
 import ClearButton from './componentes/ClearButton';
 import Display from './componentes/Display';
 
-import {useState} from 'react';
-import {evaluate} from 'mathjs'
+import { useState } from 'react';
+import { evaluate } from 'mathjs'
 
 
 function App() {
 
-  const [input,setInput] = useState('')
+  const [input, setInput] = useState('')
 
   const addInput = val => {
     setInput(input + val);
   }
 
   const result = () => {
-    if(input){
-    setInput(evaluate(input))
-  } else {
-    alert('Please, set values to calculate')
-  }
+    if (input) {
+      setInput(evaluate(input))
+    } else {
+      alert('Please, set values to calculate')
+    }
   }
 
   return (
     <div className='App'>
       <div className='calculator-container'>
-        <Display input={input}/>
+        <Display input={input} />
         <div className='fila'>
           <Button handleClick={addInput}>7</Button>
           <Button handleClick={addInput}>8</Button>
@@ -34,25 +34,25 @@ function App() {
           <Button handleClick={addInput}>+</Button>
         </div>
         <div className='fila'>
-        <Button handleClick={addInput}>4</Button>
+          <Button handleClick={addInput}>4</Button>
           <Button handleClick={addInput}>5</Button>
           <Button handleClick={addInput}>6</Button>
           <Button handleClick={addInput}>-</Button>
         </div>
         <div className='fila'>
-        <Button handleClick={addInput}>1</Button>
+          <Button handleClick={addInput}>1</Button>
           <Button handleClick={addInput}>2</Button>
           <Button handleClick={addInput}>3</Button>
           <Button handleClick={addInput}>*</Button>
         </div>
         <div className='fila'>
-        <ClearButton handleClear={() => setInput('')}>AC</ClearButton>
+          <ClearButton handleClear={() => setInput('')}>AC</ClearButton>
           <Button handleClick={addInput}>0</Button>
           <Button handleClick={addInput}>.</Button>
           <Button handleClick={addInput}>/</Button>
         </div>
         <div className='fila'>
-        <Button handleClick={result}>=</Button>
+          <Button handleClick={result}>Calcular</Button>
         </div>
       </div>
     </div>
